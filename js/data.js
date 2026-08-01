748a5687f1fe84ce76cd662a48bb2af92984449c
piiii1111 <ohmygodetet@gmail.com>
2026-08-01 23:22:27 +0800

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -1,6 +1,7 @@
-window.prototypeJourneys = [
-  {id:'tamsui',title:'淡水週末小旅行',year:2026,start:'2026-07-18',date:'2026.07.18－07.19',country:'台灣',cities:['淡水'],region:'台灣',photo:'https://images.unsplash.com/photo-1577702312706-e23ff063064f?auto=format&fit=crop&w=700&q=82'},
-  {id:'kenting',title:'墾丁三天兩夜',year:2026,start:'2026-05-01',date:'2026.05.01－05.03',country:'台灣',cities:['墾丁'],region:'台灣',photo:'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&w=700&q=82'},
-  {id:'nagoya',title:'名古屋六天五夜',year:2025,start:'2025-10-22',date:'2025.10.22－10.27',country:'日本',cities:['名古屋','常滑'],region:'東北亞',photo:'https://images.unsplash.com/photo-1528360983277-13d401cdc186?auto=format&fit=crop&w=700&q=82',review:'名古屋近郊適合親子自駕，水族館與麵包超人博物館是孩子最有記憶點的行程。下次會減少購物點，把更多時間留給郊區散步。'},
-  {id:'tokyo',title:'東京六天五夜',year:2023,start:'2023-05-01',date:'2023.05.01－05.06',country:'日本',cities:['東京'],region:'東北亞',photo:'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?auto=format&fit=crop&w=700&q=82'}
-];
+window.TRAVEL_COORDINATES={
+  "東京":[35.6762,139.6503],"東京鐵塔":[35.6586,139.7454],"名古屋":[35.1815,136.9066],"名古屋市":[35.1815,136.9066],
+  "常滑":[34.8867,136.8324],"淡水":[25.1676,121.445],"墾丁":[21.9469,120.796],"台北":[25.033,121.5654],
+  "台中":[24.1477,120.6736],"高雄":[22.6273,120.3014],"首爾":[37.5665,126.978],"曼谷":[13.7563,100.5018],
+  "新加坡":[1.3521,103.8198],"日本":[36.2048,138.2529],"韓國":[35.9078,127.7669],"台灣":[23.6978,120.9605]
+};
+window.FALLBACK_JOURNEYS=[];
